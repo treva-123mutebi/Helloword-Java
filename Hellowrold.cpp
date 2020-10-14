11cd7eb0e69b637f7781eb6c2b81cd87a1927d0c
Mutebi David Trevor <mdavid.trevor@gmail.com>
2020-10-14 12:05:31 +0300

    New C++

---
+++ Hellowrold.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string mygreeting = "Hello world";
+    cout << mygreeting;
+    return 0;
+}
---
+++ learn C/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string fullName;
+    cout << "Type your full Name";
+    getline (cin, fullName);
+
+    cout << "Your Name is " << fullName;
+    return 0;
+}
